21364b1cf7a725d139485b9701bff7ecacb31fb6
Decagon <decagon@Darot.local>
2020-10-06 09:13:04 +0100

    added intent to start gmail

--- app/src/main/java/com/trapezoidlimited/playground/MainActivity.kt
+++ app/src/main/java/com/trapezoidlimited/playground/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.trapezoidlimited.playground
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
-import androidx.core.app.ShareCompat
+import androidx.appcompat.app.AppCompatActivity
 import com.auth0.android.jwt.JWT
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,12 +14,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btn.setOnClickListener {
-            val geo = "geo:0,0?q="
-            val loc = "Lagos"
-            val linkUrl ="https://www.google.com"
-            val url = Uri.parse(linkUrl)
-            val intent = Intent(Intent.ACTION_VIEW, url)
-            startActivity(intent)
+//            val geo = "geo:0,0?q="
+//            val loc = "Lagos"
+//            val linkUrl ="https://www.google.com"
+//            val url = Uri.parse(linkUrl)
+//            val intent = Intent(Intent.ACTION_VIEW, url)
+//            startActivity(intent)
+            val intent = Intent(Intent.ACTION_MAIN)
+            intent.addCategory(Intent.CATEGORY_APP_EMAIL)
+            try {
+                startActivity(Intent.createChooser(intent, getString(R.string.open_email_app)))
+            } catch (e: ActivityNotFoundException) {
+                Log.i("Main", "No activity")
+            }
 //            if(intent.resolveActivity(packageManager) != null){
 //
 //            }
@@ -37,17 +42,17 @@ class MainActivity : AppCompatActivity() {
 //                    .startChooser()
         }
 
-        val token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7ImVtYWlsIjoiZHVyb3NvbW8uYWJkdWxyYXNhcUBnbWFpbC5jb20iLCJpc1ZlcmlmaWVkIjp0cnVlfSwiaWF0IjoxNjAxNjQ4Nzk2LCJleHAiOjE2MDE3MzUxOTZ9.KrMgA7d6cV2u9vk6vraew1a4LlOesc2J6NPBknfz-uo"
-        val jwt = JWT(token)
-        val istime = jwt.issuedAt?.time
-        val payloadString = jwt.claims.get("payload")?.asObject(Data::class.java)
+//        val token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7ImVtYWlsIjoiZHVyb3NvbW8uYWJkdWxyYXNhcUBnbWFpbC5jb20iLCJpc1ZlcmlmaWVkIjp0cnVlfSwiaWF0IjoxNjAxNjQ4Nzk2LCJleHAiOjE2MDE3MzUxOTZ9.KrMgA7d6cV2u9vk6vraew1a4LlOesc2J6NPBknfz-uo"
+//        val jwt = JWT(token)
+//        val istime = jwt.issuedAt?.time
+//        val payloadString = jwt.claims.get("payload")?.asObject(Data::class.java)
 
 
 //        val g = Gson()
 //        val str = g.toJson(payloadString)
 //        val payload = g.fromJson<Data>(str, Data::class.java)
 
-        Log.i("Main", "Issuer $payloadString, time$istime")
+//        Log.i("Main", "Issuer $payloadString, time$istime")
 
     }
 }
